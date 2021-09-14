7efb8b399e95354d193db8d9b3cd055cad8ec9f5
Luiz Henrique Santos <lhsantosazs@gmail.com>
2021-09-13 21:57:56 -0300

    Modeling database

---
+++ src/database/migrations/2021_09_09_010232_create_customer_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('login');
+            $table->string('name');
+            $table->string('cpf');
+            $table->string('email');
+            $table->string('adress');
+            $table->string('password');
+            $table->binary('doc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
